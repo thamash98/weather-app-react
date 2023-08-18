ed0f94f2557a4715daa69c3f1ec92b3f5897a302
Thamash Karunarathna <thasaranga123@gmail.com>
2023-08-18 09:38:45 +0530

    icon div staticaly add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import summer from './assets/summer.jpg';
+import winter from './assets/winter.jpg';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
+    <div className="app" style={{backgroundImage:`url(${summer})`}}> 
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter City..." />
+            <button>°F</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+             <h3>London,GB</h3>
+             <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="weatherIcon" />
+             <h3>Cloudy</h3>
+            </div>
+          </div>   
+        </div>
+      </div>
     </div>
   );
 }
